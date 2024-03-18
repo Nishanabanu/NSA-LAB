dd0c520a70e620b3654fd6c7e6e36504a3fb00b4
Nishanabanu <147145402+Nishanabanu@users.noreply.github.com>
2024-03-18 14:24:52 +0530

    COMPLETED

---
+++ prgrm4/greetings.sh
@@ -0,0 +1,20 @@
+#display a nsg to login user with greetings
+if [ $# -gt 0]
+then
+echo "syntax is $0"
+exit 1
+fi
+echo "Hello $USER..."
+h=`date +%H`
+if [ $h -lt 12 ]
+then
+echo "Good morning"
+elif [ $h -lt 16 ]
+then
+echo "Good afternoon"
+elif [ $h -lt 20 ]
+then
+echo"Good evening"
+else
+echo"GOOD NIGHT"
+fi
